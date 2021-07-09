4400e4a4a19df3f56fed6da21b6ac6bdda3cbe55
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2021-07-10 08:32:16 +0900

    simplify `data/manifest.js`

--- src/site/data/manifest.js
+++ src/site/data/manifest.js
@@ -1,15 +1,12 @@
 const fsPromises = require("fs/promises");
-const path = require("path");
 
 const isProd = process.env.ELEVENTY_ENV === "production";
-
-const manifestPath = path.join(__dirname, "../../../dist/manifest.json");
 
 module.exports = async () => {
 	let result = null;
 
 	if (isProd) {
-		result = JSON.parse(await fsPromises.readFile(manifestPath));
+		result = JSON.parse(await fsPromises.readFile("dist/manifest.json"));
 	}
 
 	return result;
