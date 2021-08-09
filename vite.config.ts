64aa9491eb476e9a22af1af7e0bbdac5ac29c77e
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2021-08-10 02:29:36 +0900

    install rollup-plugin-visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
+import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
 import { config } from "./config";
 
 export default defineConfig({
 	base: config.pathPrefix,
+	plugins: [visualizer()],
 	logLevel: "warn",
 	build: {
 		rollupOptions: {
