f49a1ede2a1e5ef56847d57c811384255ecc72d8
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2022-08-31 21:55:52 +0900

    remove dom.ts

--- src/scripts/components/dialog.ts
+++ src/scripts/components/dialog.ts
@@ -1,5 +1,3 @@
-import { forceFocus } from "../utils/dom";
-
 export default () => ({
 	isOpen: false,
 
@@ -98,4 +96,13 @@ function inactivateOutside(element: HTMLElement) {
 			element.inert = false;
 		}
 	};
+}
+
+function forceFocus(element: HTMLElement, focusOptions: FocusOptions = {}) {
+	element.focus(focusOptions);
+
+	if (document.activeElement !== element) {
+		element.tabIndex = -1;
+		element.focus(focusOptions);
+	}
 }
--- src/scripts/utils/dom.ts
+++
@@ -1,8 +0,0 @@
-export function forceFocus(element: HTMLElement, focusOptions: FocusOptions = {}) {
-	element.focus(focusOptions);
-
-	if (document.activeElement !== element) {
-		element.tabIndex = -1;
-		element.focus(focusOptions);
-	}
-}
